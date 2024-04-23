892cd76b571761754fc48aba1ec299fe4c3e21b0
RaiDeOliveira <rai.caje@sou.inteli.edu.br>
2024-04-23 00:20:49 -0300

    docs: deploy config try 8

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/2024-1B-T08-EC06-G05/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
