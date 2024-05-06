f9eac7e989916e7e1ba12cb917c3ed4d9bae7995
MarioVenturaMedeiros <mario.medeiros@sou.inteli.edu.br>
2024-05-06 14:20:32 -0300

    criacao do workspace

---
+++ workspace/src/SugarZ3ro_pkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'SugarZ3ro_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='SugarZ3ro',
+    maintainer_email='mario.medeiros@sou.inteli.edu.br',
+    description='Pacote de ROS2 para controle do turtlebot3 burger',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "SugarZ3ro_pkg = SugarZ3ro_pkg.SugarZ3ro_pkg:main",
+        ],
+    },
+)
