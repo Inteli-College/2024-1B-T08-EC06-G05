a4befa3c7eb430feceeabe35d71f76d8a3810d5e
gustavoesteves0 <gustavo.esteves@sou.inteli.edu.br>
2024-05-14 16:58:57 -0300

    create react and tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/App.jsx",
+    "./src/main.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
