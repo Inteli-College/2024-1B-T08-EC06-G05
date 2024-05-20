d1ef6b96cdef6c8598d54b695399464ed35dd0c0
Isabelle Oliveira <isabelle.oliveira@sou.inteli.edu.br>
2024-05-20 17:52:34 -0300

    entrega final

---
+++ src/frontend/src/components/camera/camera.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useRef } from 'react';
+import ROSLIB from 'roslib';
+import EventEmitter2 from 'eventemitter2';
+
+const VideoStream = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    // Conectar ao servidor ROS
+    const ros = new ROSLIB.Ros({
+      url: 'ws://localhost:9090'
+    });
+
+    ros.on('connection', () => {
+      console.log('Connected to websocket server.');
+    });
+
+    ros.on('error', (error) => {
+      console.log('Error connecting to websocket server: ', error);
+    });
+
+    ros.on('close', () => {
+      console.log('Connection to websocket server closed.');
+    });
+
+    // Assinar ao tópico de vídeo
+    const videoTopic = new ROSLIB.Topic({
+      ros: ros,
+      name: '/video_frames',
+      messageType: 'sensor_msgs/CompressedImage'
+    });
+
+    // Função para lidar com os quadros de vídeo recebidos
+    videoTopic.subscribe((message) => {
+      if (videoRef.current) {
+        videoRef.current.src = 'data:image/jpeg;base64,' + message.data;
+      }
+    });
+
+    // Limpar a assinatura quando o componente for desmontado
+    return () => {
+      videoTopic.unsubscribe();
+      ros.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Real-time Video Stream from ROS2 Topic</h1>
+      <img id="videoStream" ref={videoRef} alt="Video Stream" style={{ width: '640px', height: '480px' }} />
+    </div>
+  );
+};
+
+export default VideoStream;
--- src/frontend/src/pages/teleopScreen.jsx
+++ src/frontend/src/pages/teleopScreen.jsx
@@ -1,10 +1,13 @@
 import AiButton from "../components/aiButton/aiButton"
+import VideoStream from "../components/camera/camera"
+
 
 function TeleopScreen() {
   return (
     <>
       <h1>Teleoperação</h1>
       <AiButton />
+      <VideoStream />
     </>
   )
 }
