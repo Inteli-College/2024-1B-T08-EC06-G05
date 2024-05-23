2dac10b6c63bb4a911a4543c3440d1fa5edbf34d
gustavoesteves0 <gustavo.esteves@sou.inteli.edu.br>
2024-05-23 19:59:15 -0300

    add navigation to start button

--- src/frontend/src/pages/mainMenu.jsx
+++ src/frontend/src/pages/mainMenu.jsx
@@ -10,13 +10,16 @@ function MainMenu() {
 const infoPage = () => {
   navigate("/helpScreen")
 }
+const teleopPage = () => {
+  navigate("/teleopScreen")
+}
   return (
     <div className="bg-black h-screen flex flex-col items-center justify-center">
       <div className="mb-8">
         <img src={SugarZ3roLogo} alt="Logo SugarZ3ro" />
       </div>
       <div className='flex flex-col items-center space-y-4 md:flex-row md:space-x-10 md:mt-28'>
-        <button className="bg-white text-black py-3 px-10 rounded-full font-bold hover:bg-gray-400 mt-2 md:mt-0">Start</button>
+        <button onClick={teleopPage} className="bg-white text-black py-3 px-10 rounded-full font-bold hover:bg-gray-400 mt-2 md:mt-0">Start</button>
         <button className="bg-transparent text-white border-none hover:text-slate-400 mt-4 md:mt-0" style={{ borderRadius: '100%', borderWidth: '2px'}}>
             <FontAwesomeIcon onClick={() => infoPage()}icon={faCircleInfo} style={{ fontSize: '2.5rem'}}/>
         </button>
