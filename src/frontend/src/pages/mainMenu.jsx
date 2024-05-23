100a63d8f34c87e183cdba82f480299c606a12a9
gustavoesteves0 <gustavo.esteves@sou.inteli.edu.br>
2024-05-23 19:44:06 -0300

    add navigation to mainMenu (going to info page only)

--- src/frontend/src/pages/mainMenu.jsx
+++ src/frontend/src/pages/mainMenu.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 import SugarZ3roLogo from '../assets/logo-sugarz3ro.svg';
+import { useNavigate } from 'react-router-dom'
 
-const MainMenu = () => {
+function MainMenu() {
+  const navigate = useNavigate()
+
+const infoPage = () => {
+  navigate("/helpScreen")
+}
   return (
     <div className="bg-black h-screen flex flex-col items-center justify-center">
       <div className="mb-8">
@@ -12,7 +18,7 @@ const MainMenu = () => {
       <div className='flex flex-col items-center space-y-4 md:flex-row md:space-x-10 md:mt-28'>
         <button className="bg-white text-black py-3 px-10 rounded-full font-bold hover:bg-gray-400 mt-2 md:mt-0">Start</button>
         <button className="bg-transparent text-white border-none hover:text-slate-400 mt-4 md:mt-0" style={{ borderRadius: '100%', borderWidth: '2px'}}>
-            <FontAwesomeIcon icon={faCircleInfo} style={{ fontSize: '2.5rem'}}/>
+            <FontAwesomeIcon onClick={() => infoPage()}icon={faCircleInfo} style={{ fontSize: '2.5rem'}}/>
         </button>
       </div>
     </div>
