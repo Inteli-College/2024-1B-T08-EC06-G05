c7ff22b24f7957c4974001d854e464ea20139023
RaiDeOLiveira <rai.caje@sou.inteli.edu.br>
2024-05-24 10:45:14 -0300

    detection update test

--- src/workspace/src/SugarZ3ro_pkg/SugarZ3ro_pkg/obstacle_detection.py
+++ src/workspace/src/SugarZ3ro_pkg/SugarZ3ro_pkg/obstacle_detection.py
@@ -1,20 +1,29 @@
-# obstacle_detection.py
-
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 
 class ObstacleDetection(Node):
     def __init__(self):
         super().__init__('obstacle_detection')
-        self.scan_subscriber = self.create_subscription(LaserScan, 'scan', self.scan_callback, 10)
+
+        qos_profile = QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT
+        )
+
+        self.scan_subscriber = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.scan_callback,
+            qos_profile
+        )
+
         self.obstacle_publisher = self.create_publisher(Bool, 'obstacle_detected', 10)
         self.stop_distance = 0.3  # 30 cm
         self.get_logger().info("Nó de detecção de obstáculos iniciado")
 
     def scan_callback(self, msg):
-
         ranges = [distance for distance in msg.ranges if not distance == float('inf')]
 
         if ranges:
@@ -24,7 +33,6 @@ class ObstacleDetection(Node):
             if min_distance <= self.stop_distance:
                 self.obstacle_publisher.publish(Bool(data=True))
                 self.get_logger().warn("Obstáculo detectado a 30cm!")
-
             else:
                 self.obstacle_publisher.publish(Bool(data=False))
 
