1ba8237109a53172351da0a25ad8adf14c04b545
RaiDeOLiveira <rai.caje@sou.inteli.edu.br>
2024-05-24 11:34:25 -0300

    undo qos value update

--- src/workspace/src/SugarZ3ro_pkg/SugarZ3ro_pkg/obstacle_detection.py
+++ src/workspace/src/SugarZ3ro_pkg/SugarZ3ro_pkg/obstacle_detection.py
@@ -1,40 +1,45 @@
+# obstacle_detection.py
+
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist
 
 class ObstacleDetection(Node):
     def __init__(self):
         super().__init__('obstacle_detection')
 
-        qos_profile = QoSProfile(
-            reliability=QoSReliabilityPolicy.BEST_EFFORT
-        )
-
         self.scan_subscriber = self.create_subscription(
             LaserScan,
             'scan',
             self.scan_callback,
-            qos_profile
-        )
+            10)
 
-        self.obstacle_publisher = self.create_publisher(Bool, 'obstacle_detected', 10)
         self.stop_distance = 0.3  # 30 cm
-        self.get_logger().info("Nó de detecção de obstáculos iniciado")
 
+        self.vel_publisher = self.create_publisher(
+            Twist,
+            'cmd_vel',
+            10)
+        self.get_logger().info("Nó de de parada")
+    
     def scan_callback(self, msg):
+
         ranges = [distance for distance in msg.ranges if not distance == float('inf')]
+
+        msg_parada = Twist()
+        msg_parada.angular.z = 0.0
+        msg_parada.linear.x = 0.0
 
         if ranges:
             min_distance = min(ranges)
             self.get_logger().info(f"Menor distância: {min_distance}")
 
             if min_distance <= self.stop_distance:
-                self.obstacle_publisher.publish(Bool(data=True))
                 self.get_logger().warn("Obstáculo detectado a 30cm!")
-            else:
-                self.obstacle_publisher.publish(Bool(data=False))
+                self.vel_publisher.publish(msg_parada)
+                print("PARANDO O ROBÔ")
 
 def main(args=None):
     rclpy.init(args=args)
